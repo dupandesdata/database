e1c040b1c844c0184bb8650dd2fe7e2d4dafbf31
dupandesdata <46831379+dupandesdata@users.noreply.github.com>
2022-10-24 15:10:07 +0700

    Create develop.js

---
+++ web/develop.js
@@ -0,0 +1,13 @@
+document.getElementById("container").innerHTML = "
+<nav class="top-nav">
+  <li>Home</li>
+  <li>Home</li>
+  <li>Home</li>
+</nav>
+";
+document.getElementById("containerB").innerHTML = "
+
+<div class="footer">
+  <p>2022</p>
+</div>
+";
