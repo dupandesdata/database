c4b6a089bff3c67b575b3149f5638c14ad555e29
dupandesdata <46831379+dupandesdata@users.noreply.github.com>
2022-10-25 06:02:39 +0700

    Update develop.js

--- web/develop.js
+++ web/develop.js
@@ -1,3 +1,3 @@
 document.getElementById("container").innerHTML = "<h1>Catatan Pembelajaran Saya</h1>";
 document.getElementById("containerB").innerHTML = "Copyright 2022";
-document.querySelector(".container-post").innerHTML = "<h1>Catatan Pembelajaran Saya</h1>";
+document.querySelector(".containerTop").innerHTML = "Catatan Pembelajaran Saya";
