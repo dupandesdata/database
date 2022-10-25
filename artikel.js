221a95ff46a68f0ad168d6a1abc583c78b606b30
dupandesdata <46831379+dupandesdata@users.noreply.github.com>
2022-10-25 15:49:47 +0700

    Update and rename artikel/html.js to artikel.js

---
+++ artikel.js
@@ -0,0 +1,70 @@
+export let domain = "https://masdupan.com/"
+let databaseHtml = [
+  {
+    data: {
+      id: "html0",
+      artikel: "",
+      link: "",
+    }
+  },
+  {
+    data: {
+      id: "html1",
+      artikel: "Apa Itu Internet",
+      link: "html1.html",
+      folder: "materi/"
+    }
+  },
+  {
+    data: {
+      id: "html2",
+      artikel: "Domain dan DNS Server",
+      link: "domain-dan-dns-server.html",
+      folder: "materi/"
+    }
+  },
+  {
+    data: {
+      id: "html3",
+      artikel: "Pengenalan Web Hosting",
+      link: "pengenalan-web-hosting.html",
+      folder: "materi/"
+    }
+  }
+];
+
+let databaseCss = [
+  {
+    data: {
+      id: "css0",
+      artikel: "",
+      link: "",
+    }
+  },
+  {
+    data: {
+      id: "css1",
+      artikel: "Pengertian Struktur Dasar",
+      link: "pengertian-struktur-dasar.html",
+      folder: "materi/"
+    }
+  },
+  {
+    data: {
+      id: "css2",
+      artikel: "Pengertian Selector Dasar",
+      link: "pengertian-selector-dasar.html",
+      folder: "materi/"
+    }
+  },
+  {
+    data: {
+      id: "css3",
+      artikel: "mengenal Property dan Value",
+      link: "mengenal-property-dan-value.html",
+      folder: "materi/"
+    }
+  }
+];
+export default databaseHtml;
+export default databaseCss;
