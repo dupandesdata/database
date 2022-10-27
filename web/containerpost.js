2e338e08fae980aabbb0ef611ef5c024b2e9fb73
dupandesdata <46831379+dupandesdata@users.noreply.github.com>
2022-10-27 19:24:04 +0700

    Update containerpost.js

--- web/containerpost.js
+++ web/containerpost.js
@@ -1,2 +1,5 @@
-document.querySelector(".containerTop").innerHTML = "pembelajaran Saya";
+
+    let myHeader = "<section class='myHeader'>"
+    let header = "<header>Dupan Code</header>"
+document.querySelector(".containerTop").innerHTML = myHeader+header+"/<section>"
 document.querySelector(".containerBottom").innerHTML = "footer";
