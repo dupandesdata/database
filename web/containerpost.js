0445b12e06cd2dcb7dc4c7855e68c6a04efbf39a
dupandesdata <46831379+dupandesdata@users.noreply.github.com>
2022-10-28 09:23:21 +0700

    Update containerpost.js

--- web/containerpost.js
+++ web/containerpost.js
@@ -1,5 +1,5 @@
 
     let myHeader = "<section class='myHeader'>"
     let header = "<header>Dupan Code</header>"
-document.querySelector(".containerTop").innerHTML = myHeader+header+"/<section>"
+document.querySelector(".containerTop").innerHTML = myHeader+header
 document.querySelector(".containerBottom").innerHTML = "footer";
