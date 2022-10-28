5f3536d1103012ce7f770fee94fd099fef918e77
dupandesdata <46831379+dupandesdata@users.noreply.github.com>
2022-10-28 10:29:11 +0700

    Update homeskill.js

--- web/homeskill.js
+++ web/homeskill.js
@@ -6,32 +6,32 @@ let databaseHtml = [
       id: "html0",
       artikel: "",
       link: "",
-    },
+    }
   },
   {
     data: {
       id: "html1",
       artikel: "Apa Itu Internet",
       link: "html1.html",
-      folder: "materi/",
-    },
+      folder: "materi/"
+    }
   },
   {
     data: {
       id: "html2",
       artikel: "Domain dan DNS Server",
-      link: "domain-dan-dns-server.html",
-      folder: "materi/",
-    },
+      link: "html2.html",
+      folder: "materi/"
+    }
   },
   {
     data: {
       id: "html3",
       artikel: "Pengenalan Web Hosting",
-      link: "pengenalan-web-hosting.html",
-      folder: "materi/",
-    },
-  },
+      link: "html3.html",
+      folder: "materi/"
+    }
+  }
 ];
 
 let databaseCss = [
@@ -40,32 +40,32 @@ let databaseCss = [
       id: "css0",
       artikel: "",
       link: "",
-    },
+    }
   },
   {
     data: {
       id: "css1",
       artikel: "Pengertian Struktur Dasar",
-      link: "pengertian-struktur-dasar.html",
-      folder: "materi/",
-    },
+      link: "css1.html",
+      folder: "materi/"
+    }
   },
   {
     data: {
       id: "css2",
       artikel: "Pengertian Selector Dasar",
-      link: "pengertian-selector-dasar.html",
-      folder: "materi/",
-    },
+      link: "css2.html",
+      folder: "materi/"
+    }
   },
   {
     data: {
       id: "css3",
       artikel: "mengenal Property dan Value",
-      link: "mengenal-property-dan-value.html",
-      folder: "materi/",
-    },
-  },
+      link: "css3.html",
+      folder: "materi/"
+    }
+  }
 ];
 // variabel
 let materiHtml = document.getElementById("html0");
