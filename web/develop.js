a3d6afd36b09b5c435ffde6e7957e3dae9041dc6
Dupandes Melinium <46831379+dupandesdata@users.noreply.github.com>
2022-11-02 10:37:16 +0700

    Update develop.js

--- web/develop.js
+++ web/develop.js
@@ -1,5 +1,6 @@
-document.getElementById("container").innerHTML = "<h1>Catatan Pembelajaran Saya</h1>";
 document.getElementById("containerB").innerHTML = "Copyright 2022";
+
+// Sertifikat
 const mediaQuery = window.matchMedia('(max-width: 768px)');
 const img = "<img class='sertifikat-img' src= '"
 let skillHtml = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEioQqurj_U9nI_WmZGnd-LHWkUVZCPqk3xbSQdER5ACDK9pKmIveaOh7HcCYUlktsk4N81VOfenrjGsDgvfSVB_oCAaZ6mcUGiyZiPChC30o0TzKZ5l_G9zkuJtRxgt4yLoTr5FF1Hz7rhdUcITNXsw6P79L2t3nTf6rCQFtIj1mS9IfsNfMLcZkIDoHA/s320/HTML_certificate%20%281%29.jpg"
@@ -9,9 +10,8 @@ if (mediaQuery.matches) {
 }
 else {
   document.querySelector('#imagesHtml').innerHTML = img + skillHtml + "'>" + "<p>HTML Dasar </p>"
-  document.querySelector('#imagesCss').innerHTML = img + skillCss + "'>" + "<p>HTML Dasar </p>"
+  document.querySelector('#imagesCss').innerHTML = img + skillCss + "'>" + "<p>CSS Dasar </p>"
 }
-
 
 // Sidebar Home
 const allButtons = document.querySelectorAll("button.dropbtn");
