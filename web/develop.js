3c80a619658bd2a66f4327be7187e9f59618a6fe
Dupandes Melinium <46831379+dupandesdata@users.noreply.github.com>
2022-11-14 09:21:23 +0700

    Update develop.js

--- web/develop.js
+++ web/develop.js
@@ -1,26 +1,19 @@
-document.getElementById("containerB").innerHTML = "Copyright 2022";
-
-// Sertifikat
-const mediaQuery = window.matchMedia('(max-width: 768px)');
-const img = "<img class='sertifikat-img' src= '"
-let skillHtml = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEioQqurj_U9nI_WmZGnd-LHWkUVZCPqk3xbSQdER5ACDK9pKmIveaOh7HcCYUlktsk4N81VOfenrjGsDgvfSVB_oCAaZ6mcUGiyZiPChC30o0TzKZ5l_G9zkuJtRxgt4yLoTr5FF1Hz7rhdUcITNXsw6P79L2t3nTf6rCQFtIj1mS9IfsNfMLcZkIDoHA/s320/HTML_certificate%20%281%29.jpg"
-let skillCss = ""
-if (mediaQuery.matches) {
-  document.querySelector('#imagesHtml-mobile').innerHTML = img + skillHtml + "'>" + "<p>HTML Dasar </p>"
-}
-else {
-  document.querySelector('#imagesHtml').innerHTML = img + skillHtml + "'>" + "<p>HTML Dasar </p>"
-  document.querySelector('#imagesCss').innerHTML = img + skillCss + "'>" + "<p>CSS Dasar </p>"
-}
-
-// Sidebar Home
-const allButtons = document.querySelectorAll("button.dropbtn");
-allButtons.forEach((btn) => {
-  btn.onclick = () => {
-    allButtons.forEach((btn_X) => {
-      let divContent = btn_X.nextElementSibling;
-      if (btn === btn_X) divContent.classList.toggle("show");
-      else divContent.classList.remove("show");
-    });
+const z = document.querySelectorAll("#skill, #html, #css, #js");
+for (let a = 0; a < z.length; a++) {
+  z[0].onclick = function (myTap) {
+    var target = myTap.target;
+    if (target.id === "btn-html") {
+      z[1].style.display = "block";
+    } else if (target.id === "btn-css") {
+      z[2].style.display = "block";
+    } else if (target.id === "btn-js") {
+      z[3].style.display = "block";
+    } else if (target.id === "close-html") {
+      z[1].style.display = "none";
+    } else if (target.id === "close-css") {
+      z[2].style.display = "none";
+    } else if (target.id === "close-js") {
+      z[3].style.display = "none";
+    }
   };
-});
+}
